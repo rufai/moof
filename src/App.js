f993b168b3f98f1acb81cfb7413d66b50a65ab77
Rufai Mustapha <rufai.mustopha@gmail.com>
2021-03-09 12:21:00 +0100

    removed extra routes from app

--- src/App.js
+++ src/App.js
@@ -17,32 +17,21 @@ export default function App(props) {
       <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
+         
             <li>
               <Link to="/about">About</Link>
             </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/users">Users Again</Link>
-            </li>
+        
+            
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-          <About task={props.tasks} />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
+       
           <Route path="/">
-            <Home />
+            <About task={props.tasks} />
           </Route>
         </Switch>
       </div>
